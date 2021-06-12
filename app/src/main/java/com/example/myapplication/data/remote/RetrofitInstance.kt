eeacbcf7cca3e772b86d0eca76e693006c5ac471
kirti <kashyap.kirti11@gmail.com>
2021-06-12 16:08:22 +0100

    retrofit instance

---
+++ app/src/main/java/com/example/myapplication/data/remote/RetrofitInstance.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication.data.remote
+
+
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance {
+
+    companion object {
+
+        private val retrofit by lazy {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+            Retrofit.Builder()
+                .baseUrl("https://armago-staging.herokuapp.com/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+        }
+
+        val api: ApiService by lazy {
+            retrofit.create(ApiService::class.java)
+        }
+    }
+
+}
